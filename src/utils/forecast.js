1d554db3d22b86ec962dfa96fba3317cad9043a1
Famojuro Adeniyi <niyi.famojuro@naijacowry.com>
2019-11-10 19:17:27 +0100

    Added more data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
                 callback('Cordinate error', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain. The highest temperature for the day is ${body.daily.data[0].temperatureHigh}`)
         }
     })
 }
